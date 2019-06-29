628d72883d74fc7dd61df208f6496991bc6ffaa2
phenomenal-NazB <abanichinaza@gmail.com>
2019-06-29 14:50:05 +0100

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abani blessing nazb\desktop\djangogirls\blessing\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
